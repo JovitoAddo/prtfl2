f46ab8c8fb7cc98338677ea8418e22ce722dbfcb
JovitoAddo <jovitoaddo@gmail.com>
2022-09-20 20:24:44 +0700

    first commit 2nd port

---
+++ src/Styled/DivStyled.js
@@ -0,0 +1,20 @@
+import tw from "tailwind-styled-components";
+
+export const Container = tw.div`
+    flex
+   
+    text-lg
+    justify-center
+    p-6
+    
+`
+
+export const Container2 = tw.div`
+    grid
+    justify-center
+    bg-customblack
+    p-10
+    border-white
+    border-4
+    
+`
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors:{
+      "customwhite":'#F5F5F5',
+      "customred":'#F05454',
+      "customblue":'#30475E',
+      "customblack":'#121212',
+    },
+    fontFamily:{
+      merriwheater:['Merriweather', 'serif'],
+      poiret:['Poiret One', 'cursive'],
+    },
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
+};
