6dee2362f82a2c7042e851273677caa70e20bbfc
JovitoAddo <jovitoaddo@gmail.com>
2022-10-24 18:00:44 +0700

    add stuff

--- src/Components/Contents.js
+++ src/Components/Contents.js
@@ -32,20 +32,20 @@ const Contents = () => {
         />
         <div className="absolute top-56  object-cover bg-transparent grid justify-items-center">
           <img src={logo} className=" h-80 w-80 " />
-          <h1 className="prose text-center font-staat text-customwhite pt-2 text-4xl bg-transparent">
+          <h1 className="prose text-center font-staat text-customwhite pt-2 text-4xl bg-transparent mb-2">
             Customize yourself according to what you wants
           </h1>
         </div>
       </div>
 
       {/* BODY */}
-      <div className="flex mx-auto justify-center prose border-b-8 min-w-full border-t-8 border-customred">
+      <div className="flex mx-auto justify-center prose border-b-8 min-w-full border-t-8 mb-2 border-customred">
         <motion.h1 className=" text-center text-5xl transition ">
           SOMETHING SOMETHING SALES?????
         </motion.h1>
       </div>
 
-      <div className="grid sm:flex justify-items-center justify-around bg-customblue p-2">
+      <div className="grid sm:flex justify-items-center justify-around bg-gradient-to-r from-customblack to-customblue p-2">
         <div className=" w-60 sm:w-80 rounded-lg">
           <Swiper
             loop={true}
@@ -54,7 +54,7 @@ const Contents = () => {
               disableOnInteraction: true,
             }}
             modules={[Autoplay]}
-            className="mySwiper  rounded-lg"
+            className="mySwiper rounded-lg bg-transparent"
           >
             <SwiperSlide>
               <img src={bp1} />
@@ -67,12 +67,17 @@ const Contents = () => {
             </SwiperSlide>
           </Swiper>
         </div>
-        <div className="prose text-center grid items-center bg-transparent text-customwhite">
-          <div className="bg-transparent">
+        <div className="prose text-center grid items-center bg-transparent text-customwhite m-2">
+          <div className="bg-transparent box-decoration-slice ">
             Est reprehenderit Lorem minim pariatur commodo. Pariatur voluptate
             id sint irure ex ipsum non tempor mollit laboris incididunt. Elit
             tempor laboris sit ipsum enim tempor commodo consequat adipisicing
-            elit velit.
+            elit velit. Sunt id dolor minim irure excepteur esse Lorem deserunt
+            ea. Ullamco proident exercitation est minim sit officia eu tempor.
+            Aliquip fugiat veniam ex aliquip anim ut commodo deserunt officia
+            sit cillum est. Laborum adipisicing anim mollit culpa quis commodo
+            do aute irure deserunt dolor officia duis ullamco. Eu cillum labore
+            ipsum voluptate eiusmod cupidatat et velit velit est sit laborum.
           </div>
         </div>
       </div>
@@ -108,8 +113,6 @@ const Contents = () => {
           </Swiper>
         </div>
       </div>
-
-      
     </container>
   );
 };
