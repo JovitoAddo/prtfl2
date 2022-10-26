6c2f539b96e041f6bf9bb9d1a05fb9e3f6d9125f
JovitoAddo <jovitoaddo@gmail.com>
2022-10-26 19:38:23 +0700

    adding animation

--- src/Components/Contents.js
+++ src/Components/Contents.js
@@ -25,7 +25,7 @@ const Contents = () => {
           loop
         />
         <div className="absolute top-56  object-cover bg-transparent grid justify-items-center">
-          <img src={logo} className=" h-80 w-80 " />
+          <img src={logo} className=" h-80 w-80 " alt="" />
           <h1 className="prose text-center font-staat text-customwhite pt-2 text-4xl bg-transparent mb-2">
             Customize yourself according to what you wants
           </h1>
@@ -37,20 +37,20 @@ const Contents = () => {
         delay={1}
         className="flex min-w-full h-40 justify-center prose border-b-8 border-t-8 mb-2 bg-white border-customred"
       >
-        <img className="w-36 bg-white p-2" src={Brand.logoAdidas4} />
-        <img className="w-36 bg-white p-2" src={Brand.logoAdidas} />
-        <img className="w-36 bg-white p-2" src={Brand.logoHugoBoss} />
-        <img className="w-36 bg-white p-2" src={Brand.logoChanel} />
-        <img className="w-36 bg-white p-2" src={Brand.logoDG} />
-        <img className="w-36 bg-white p-2" src={Brand.logoFila} />
-        <img className="w-36 bg-white p-2" src={Brand.logoJordan} />
-        <img className="w-36 bg-white p-2" src={Brand.logoNike} />
-        <img className="w-36 bg-white p-2" src={Brand.logoLacoste} />
-        <img className="w-36 bg-white p-2" src={Brand.logoKFC} />
-        <img className="w-36 bg-white p-2" src={Brand.logoPrada} />
-        <img className="w-36 bg-white p-2" src={Brand.logoPuma} />
-        <img className="w-36 bg-white p-2" src={Brand.logoUA} />
-        <img className="w-36 bg-white p-2" src={Brand.logoVersace} />
+        <img className="w-36 bg-white p-2" src={Brand.logoAdidas4} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoAdidas} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoHugoBoss} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoChanel} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoDG} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoFila} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoJordan} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoNike} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoLacoste} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoKFC} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoPrada} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoPuma} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoUA} alt="" />
+        <img className="w-36 bg-white p-2" src={Brand.logoVersace} alt="" />
       </Marquee>
 
       {/* slideshow 1 */}
@@ -67,21 +67,33 @@ const Contents = () => {
               className="mySwiper rounded-lg bg-transparent"
             >
               <SwiperSlide>
-                <img src={Models.bp1} />
+                <img src={Models.bp1} alt="" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src={Models.bp2} />
+                <img src={Models.bp2} alt="" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src={Models.bp3} />
+                <img src={Models.bp3} alt="" />
               </SwiperSlide>
             </Swiper>
           </div>
-          <div className="prose text-center grid justify-items-center bg-transparent text-customblue m-2">
-            <h1 className="text-4xl text-customred grid items-end ">
+          <div className="prose text-center grid justify-items-center bg-transparent text-customblue m-2 p-4">
+            <motion.h1
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: false, amount: 0.3 }}
+              transition={{ duration: 1, delay: 0.7 }}
+              className="text-4xl text-customred grid items-end "
+            >
               Sporty|Casual
-            </h1>
-            <div className="bg-transparent box-decoration-slice">
+            </motion.h1>
+            <motion.div
+              initial={{ y: -50, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              viewport={{ once: false, amount: 0.3 }}
+              transition={{ duration: 1, delay: 1.5 }}
+              className="bg-transparent box-decoration-slice"
+            >
               Est reprehenderit Lorem minim pariatur commodo. Pariatur voluptate
               id sint irure ex ipsum non tempor mollit laboris incididunt. Elit
               tempor laboris sit ipsum enim tempor commodo consequat adipisicing
@@ -92,7 +104,7 @@ const Contents = () => {
               culpa quis commodo do aute irure deserunt dolor officia duis
               ullamco. Eu cillum labore ipsum voluptate eiusmod cupidatat et
               velit velit est sit laborum.
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
@@ -100,18 +112,36 @@ const Contents = () => {
       {/* slideshow2 */}
       <div className="grid sm:flex justify-items-center bg-customwhite justify-around p-2">
         <div className="border-t-2 border-customblue grid sm:flex justify-items-center justify-around p-2">
-          <div className="prose text-center grid justify-items-center bg-transparent text-customblue">
-            <h1 className="text-4xl text-customred grid items-end">
+          <div className="prose text-center grid justify-items-center bg-transparent text-customblue order-last sm:order-first p-4">
+            <motion.h1
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: false, amount: 0.3 }}
+              transition={{ duration: 1, delay: 0.7 }}
+              className="text-4xl text-customred grid items-end"
+            >
               Street|Outrider
-            </h1>
-            <div className="bg-transparent">
+            </motion.h1>
+            <motion.div
+              initial={{ y: -50, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              viewport={{ once: false, amount: 0.3 }}
+              transition={{ duration: 1, delay: 1.5 }}
+              className="bg-transparent "
+            >
               Est reprehenderit Lorem minim pariatur commodo. Pariatur voluptate
               id sint irure ex ipsum non tempor mollit laboris incididunt. Elit
               tempor laboris sit ipsum enim tempor commodo consequat adipisicing
-              elit velit.
-            </div>
+              elit velit. Fugiat dolor est cupidatat laborum duis sit
+              consectetur incididunt Lorem laboris dolore ipsum irure mollit.
+              Culpa exercitation qui eu commodo dolore excepteur consequat
+              dolore do laboris. Voluptate occaecat ea minim minim enim qui
+              voluptate nisi velit incididunt cupidatat ex. Deserunt ut dolore
+              reprehenderit incididunt. Nulla est anim pariatur do ullamco do
+              nisi sit do minim.
+            </motion.div>
           </div>
-          <div className=" w-60 sm:w-80 rounded-lg border-t-2">
+          <div className=" w-60 sm:w-80 rounded-lg border-t-2 order-first sm:order-last">
             <Swiper
               loop={true}
               autoplay={{
@@ -122,13 +152,13 @@ const Contents = () => {
               className="mySwiper  rounded-lg"
             >
               <SwiperSlide>
-                <img src={Models.iw1} />
+                <img src={Models.iw1} alt="" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src={Models.iw2} />
+                <img src={Models.iw2} alt="" />
               </SwiperSlide>
               <SwiperSlide>
-                <img src={Models.iw3} />
+                <img src={Models.iw3} alt="" />
               </SwiperSlide>
             </Swiper>
           </div>
