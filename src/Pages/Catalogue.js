5e1f47826a0d4eb789a7e7d6d01c0743edad6f86
JovitoAddo <jovitoaddo@gmail.com>
2022-11-04 08:03:52 +0700

    starting catalogue page

--- src/Pages/Catalogue.js
+++ src/Pages/Catalogue.js
@@ -10,14 +10,34 @@ import "swiper/css";
 const Catalogue = () => {
   return (
     <>
-      <div className="flex justify-center pt-20">
-        <div className="grid items-center w-1/3 h-36 border-2 border-customblack rounded-md bg-gradient-to-r from-customblue to-customred">
-          <h1 className="bg-transparent text-customwhite text-5xl font-staat text-center">New Arrival Today</h1>
+      <div className="flex justify-center">
+        <div className="grid items-end w-full h-36 border-2 border-customblack bg-gradient-to-r from-customblue to-customred">
+          <h1 className="bg-transparent text-customwhite text-5xl font-staat text-center">
+            New Arrival Today
+          </h1>
         </div>
       </div>
 
-      <div className="min-h-screen grid justify-center items-center">
-        something
+      <div className="min-h-screen flex">
+        <div className="grid p-2">
+          <div className="h-96 prose text-center mx-auto grid items-center">
+            Labore deserunt magna velit consectetur tempor in cupidatat
+            excepteur. Minim eu aliqua cupidatat nostrud cupidatat sit. Nisi
+            labore qui nostrud veniam Lorem ullamco magna pariatur officia.
+            Culpa elit reprehenderit minim commodo tempor magna id ipsum
+            excepteur irure duis irure adipisicing. Incididunt aute cupidatat
+            sint laboris sint dolor est nisi fugiat occaecat enim elit laborum.
+            Sunt eu excepteur cillum fugiat sint. Nisi incididunt esse proident
+            voluptate irure Lorem adipisicing ipsum Lorem consequat esse do
+            tempor.
+          </div>
+          <div>
+            <img src={Models.cat2} alt=''/>
+          </div>
+        </div>
+        <div>
+          <img src={Models.np1} alt="" className="h-96" />
+        </div>
       </div>
     </>
   );
