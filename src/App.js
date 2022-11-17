8c2c1b1ac12b249ff94c894ec24a049435df763f
JovitoAddo <jovitoaddo@gmail.com>
2022-11-17 20:43:59 +0700

    starting login page, trying formik

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import Catalogue from "./Pages/Catalogue";
 import NotFound from "./Pages/NotFound";
 import ItemPage from "./Pages/ItemPage";
+import Login from "./Pages/Login";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/catalogue' element={<Catalogue/>}/>
           <Route path='/items' element={<ItemPage/>}/>
           <Route path='*' element={<NotFound/>}/>
+          <Route path='/login' element={<Login/>}/>
       </Routes>
       <Footer/>
     </div>
--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode><Provider store={store}>
-    <BrowserRouter>
-      <ScrollToTop />
-      
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop />
         <App />
-      
-    </BrowserRouter></Provider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
